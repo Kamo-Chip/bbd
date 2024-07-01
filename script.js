6ed77142383930760c07d429c0202acce8e13244
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2024-07-01 11:14:04 +0200

    style

--- script.js
+++ script.js
@@ -4,22 +4,16 @@ const startButton = document.getElementById("startButton");
 
 function handleOrientation(event) {
   const alphaSpan = document.querySelector("#alpha");
-  alphaSpan.textContent = event.alpha;
-  alphaSpan.innerHTML = event.alpha;
+  alphaSpan.textContent = event.alpha.toFixed(2); // Display alpha value with 2 decimal places
 }
 
 function onClick() {
-  if (typeof DeviceMotionEvent.requestPermission === "undefined") {
-    window.alert("NO orientation");
-    return;
-  }
-
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
+  if (typeof DeviceOrientationEvent.requestPermission === "function") {
     // Handle iOS 13+ devices.
-    DeviceMotionEvent.requestPermission()
+    DeviceOrientationEvent.requestPermission()
       .then((state) => {
         if (state === "granted") {
-          window.addEventListener("devicemotion", handleOrientation);
+          window.addEventListener("deviceorientation", handleOrientation);
         } else {
           console.error("Request to access the orientation was rejected");
         }
@@ -27,7 +21,8 @@ function onClick() {
       .catch(console.error);
   } else {
     // Handle regular non iOS 13+ devices.
-    window.addEventListener("devicemotion", handleOrientation);
+    window.addEventListener("deviceorientation", handleOrientation);
   }
 }
+
 startButton.addEventListener("click", onClick);
